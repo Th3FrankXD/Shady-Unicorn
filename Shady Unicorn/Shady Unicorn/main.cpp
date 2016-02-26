90460fa0e5a79b67c1b7a763e74b4cc18eb77fac
frank de Weers <frank de Weers>
2016-02-26 17:14:48 +0100

    basic sfml setup

--- Shady Unicorn/Shady Unicorn/main.cpp
+++ Shady Unicorn/Shady Unicorn/main.cpp
@@ -1,8 +1,22 @@
-#include <stdio.h>
+#include <iostream>
+#include <SFML/Window.hpp>
 
 int main()
 {
-	printf("42");
-	while (1);
+	sf::Window window(sf::VideoMode(1280, 720), "My window");
+
+	// run the program as long as the window is open
+	while (window.isOpen())
+	{
+		// check all the window's events that were triggered since the last iteration of the loop
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			// "close requested" event: we close the window
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+	}
+
 	return 0;
 }
